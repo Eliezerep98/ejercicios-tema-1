9f7177e37942240c643cb9754902124ddbb47509
Eliezer Emilio Pérez <93295326+Eliezerep98@users.noreply.github.com>
2023-01-21 16:07:22 -0300

    Add files via upload

---
+++ ejercicios tema 1/src/open/bootcamp/Main.java
@@ -0,0 +1,44 @@
+/*
+Crea un proyecto de Java desde 0.
+
+Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+ */
+
+package open.bootcamp;
+public class Main {
+    public static void main(String[] args) {
+
+        // datos numericos enteros:
+        byte variable1 = 8;
+        short variable2 = 10;
+        int variable3 = 20;
+        long variable4 = 98843123;
+        long variable5 = 1145667894;
+
+        // datos numericos decimales:
+        float variable6 = 5.5f;
+        double variable7 = 80.99d;
+
+        // datos booleanos:
+        boolean variable8 = true;
+        boolean variable9 = false;
+
+        // datos para texto:
+        char variable10 = 'b';
+        String variable11 = "Hola, soy Eliezer";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+        System.out.println(variable11);
+    }
+}
